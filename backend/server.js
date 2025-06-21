ab5fa2850bc70fe4360dd58dc8e2b2726ee5b411
Emmanuel Mulliez <manu.mulliez@gmail.com>
2025-06-21 12:16:44 +0100

    AJOUT DE DESIGN

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const app = express();
 app.use(cors());
 
-const app = express();
+
 app.use(express.json());
 app.use(cors()); // autoriser les appels depuis le frontend
 const multer = require('multer');
